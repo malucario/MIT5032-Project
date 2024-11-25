42d566c1bf15b4f77dd872824f5a5514f214d1c8
malucario <122913162+malucario@users.noreply.github.com>
2024-11-25 11:46:44 -0600

    Create ProjectHypothesesAnalysis.R

---
+++ ProjectHypothesesAnalysis.R
@@ -0,0 +1,29 @@
+# Load libraries
+library(tidyverse)
+library(ggplot2)
+library(ggpmisc)
+library(dplyr)
+library(grid)
+library(gridExtra)
+
+# Load data
+df <- read.csv("Public.csv")
+
+# Hypothesis 1: High-profile airlines (Delta, United, American, Southwest) have to pay
+# non-repair-related costs more often than other airlines and pay them in larger sums
+
+
+# -----------------------------------------------------------------------------
+# Hypothesis 2:
+
+
+# -----------------------------------------------------------------------------
+# Hypothesis 3:
+
+
+# -----------------------------------------------------------------------------
+# Hypothesis 4:
+
+
+# -----------------------------------------------------------------------------
+# Hypothesis 5:
